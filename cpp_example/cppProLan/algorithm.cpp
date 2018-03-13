93dc993f0b2cb54b212daaa4af4012991a26a74f
leaez <leaez@163.com>
2018-03-13 18:38:05 +0800

    review

--- cpp_example/cppProLan/algorithm.cpp
+++ cpp_example/cppProLan/algorithm.cpp
@@ -13,96 +13,182 @@
 #include <unordered_set>
 
 using namespace std;
+using namespace placeholders;
+template <typename T> 
+void printv(T &vi){
+    for (auto& x :  vi) // implicit use of v.begin() and v.end()
+        cout << x << "," ; cout << endl;
+}
+//#define MARK (printf("--------------%s (%d) - <%s>\n",__FILE__,__LINE__,__FUNCTION__)) 
+#define MARK (cout << "--------------" <<'('<<__LINE__<<')' << '<' << __FUNCTION__ << '>' <<endl) 
 
+vector<int> v{0, 1, 2, 3, 4, 5,6};
+vector<int> v1(3, 3);
+vector<int> v2(15, 4);
 
-/********************* 
- *
- * */ 
-int main()
-{
-    std::vector<int> v(10, 2);
-    std::vector<int> v1(3, 3);
-    std::vector<int> v2(15, 4);
-    std::partial_sum(v.cbegin(), v.cend(), v.begin());
-    std::cout << "Among the numbers: ";
-    std::copy(v.cbegin(), v.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
+void test_sequantial_access(){
+    MARK;
     /** all_of */ 
-    if (std::all_of(v.cbegin(), v.cend(), [](int i){ return i % 2 == 0; })) {
-        std::cout << "All numbers are even\n";
-    }
+    if (all_of(v.cbegin(), v.cend(), [](int i){ return i % 2 == 0; })) {
+        cout << "All numbers are even\n";    }
     /** none_of */ 
-    if (std::none_of(v.cbegin(), v.cend(), std::bind(std::modulus<int>(), 
-                                                     std::placeholders::_1, 2))) {
-        std::cout << "None of them are odd\n";
-    }
-    /** copy  */ 
-    std::for_each(v.begin(), v.end(), [](int &n){ n++; });
-    //std::for_each_n(v.begin(), 4, [](int &n){ n++; });
-    std::copy(v.cbegin(), v.cend(), std::ostream_iterator<int>(std::cout, " "));
+    if (none_of(v.cbegin(), v.cend(), bind(modulus<int>(),_1, 2))) {
+        cout << "None of them are odd\n";    }
+    /** any_of */ 
+    if (std::any_of(v.cbegin(), v.cend(), [](int n){return (n%3 == 0); })) {
+        std::cout << "At least one number is divisible by 3"<<endl;    }
+    /** for_each */ 
+    for_each(v.begin(), v.end(), [](int &n){ n++; });
+    printv(v);
+    //for_each_n(v.begin(), 4, [](int &n){ n++; });    printv(v); //c++17
+    cout <<"count:" << count(v.begin(), v.end(), 3)<< endl;
+    cout << "count if:" << count_if(v.begin(), v.end(), [](int i){ return i % 2 == 0; })<< endl;
+    /** find */
+    auto result1 = std::find(std::begin(v), std::end(v), 3);
+    if (result1 != std::end(v)) {  std::cout << "v contains: " << 3 << '\n';}
+    std::vector<int> t1{1, 2, 3};
+    auto result2 = std::find_end(v.begin(), v.end(), t1.begin(), t1.end());
+    if (result2 != v.end()) {   std::cout << "subsequence found\n";}
+    auto result3 = std::adjacent_find(v1.begin(), v1.end());
+    if (result3 != v1.end()) { std::cout << "matching adjacent elements\n";}
+    /** search  */ 
     string cont{"fsdfs456789dfds"};
     string ss{"456789"};
-    
-    auto serch = std::search(cont.begin(), cont.end(), ss.begin(), ss.end());
-    cout << serch-cont.end() <<"\n";
-    std::copy_n(v.begin(), 4, std::back_inserter(v1));
-    std::copy_backward(v1.begin(), v1.end(), v2.end());
-    std::copy(v2.cbegin(), v2.cend(), std::ostream_iterator<int>(std::cout, " "));
-    cout << "\n";
+    auto serch = search(cont.begin(), cont.end(), ss.begin(), ss.end());
+    if(serch != cont.end())
+        cout << "search get elements"<< serch-cont.end() <<"\n";
 
-    std::array<int, 10> sr{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
-    std::replace(sr.begin(), sr.end(), 8, 88);
-    std::copy(sr.cbegin(), sr.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
-    std::replace_if(sr.begin(), sr.end(),std::bind(std::less<int>(), std::placeholders::_1, 5), 55);
-    std::reverse(std::begin(sr), std::end(sr));
-    std::copy(sr.cbegin(), sr.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
+}
 
-    /** copy */ 
-    auto last = std::unique(sr.begin(), sr.end());
-    std::copy(sr.cbegin(), sr.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::string s1 = "The      striiiiiiiing    with many       spaces!";
-    std::cout << "before: " << s1 << '\n';
-    std::string s2;
-    std::unique_copy(s1.begin(), s1.end(), std::back_inserter(s2),
-                     [](char c1, char c2){ return c1 == 'i' && c2 == 'n'; });
-     std::cout << "after:  " << s2 << '\n';
 
-    /** partion  */ 
-     std::array<int, 9> v3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    auto is_even = [](int i){ return i % 2 == 0; };
-    std::cout.setf(std::ios_base::boolalpha);
-    std::cout << std::is_partitioned(v3.begin(), v3.end(), is_even) << ' ';
- 
-    std::partition(v3.begin(), v3.end(), is_even);
-    std::copy(v3.cbegin(), v3.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::is_partitioned(v3.begin(), v3.end(), is_even) << ' ';
- 
-    std::reverse(v3.begin(), v3.end());
-    std::copy(v3.cbegin(), v3.cend(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::is_partitioned(v3.begin(), v3.end(), is_even);
-    
-    /** product,  */ 
-    std::vector<int> a{0, 1, 2, 3, 4};
-    std::vector<int> b{5, 4, 2, 3, 1};
- 
-    int r1 = std::inner_product(a.begin(), a.end(), b.begin(), 0);
-    std::cout << "Inner product of a and b: " << r1 << '\n';
- 
-    int r2 = std::inner_product(a.begin(), a.end(), b.begin(), 0,
-                                std::plus<int>(), std::equal_to<int>());
-    std::cout << "Number of pairwise matches between a and b: " <<  r2 << '\n';
+array<int, 10> sr{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
+void test_sequantial_modify(){
+    MARK;
+    /** copy  */ 
+    std::vector<int> from_v(10);
+    std::vector<int> to_v{1,2,3};
+    std::iota(from_v.begin(), from_v.end(), 22);
+    copy(sr.cbegin(), sr.cend(), ostream_iterator<int>(cout, " ")); //only print
+    cout << '\n';
+    copy(from_v.begin(),from_v.end(), back_inserter(to_v)); printv(to_v);
+    copy_n(from_v.begin(), 4, back_inserter(to_v));  printv(to_v); //copy 4 elements
+    copy_backward(from_v.begin(), from_v.end(), to_v.end()); printv(to_v); // in backwards order 
+    move_backward(from_v.begin(), from_v.end(), to_v.end()); printv(to_v); // in backwards order 
+    printv(from_v);
+
+    /**fill */
+    fill(from_v.begin(),from_v.end(), 11); printv(from_v);
+    fill_n(from_v.begin(), 5, 22);  printv(from_v);
 
     /** transform generate */ 
-    std::cout << '\n';
-    std::string st("hello");
-    std::transform(st.begin(), st.end(), st.begin(),
-                   [](unsigned char c) { return std::toupper(c); });
-    std::cout << st;
+    string st("hello\n");
+    transform(st.begin(), st.end(), st.begin(), /** applies a function to a range of elements  */ 
+                   [](unsigned char c) { return toupper(c); });
+    cout <<st;
+    //assigns the results of successive function 
+    int n=2; 
+    generate(from_v.begin(), from_v.end(), [&n]{n++; return n*n; }); printv(from_v);
+    generate_n(from_v.begin(), 5, [n = 0] () mutable { return n++; }); printv(from_v);
+    /** remove */ 
+    string str1 = "Text with Some   Upaces12456789\n";
+    //remove(str1.begin(), str1.end(), ' ');
+    str1.erase(std::remove(str1.begin(), str1.end(), ' '), str1.end()); //erase make str short because of remove
+    cout <<str1 ;
+    str1.erase(remove_if(str1.begin(),str1.end(),[](unsigned char x){return std::isupper(x);}),str1.end());
+    cout <<str1 ;
+    string str2 = "Text with some   spaces\n";
+    remove_copy(str2.begin(), str2.end(),std::ostream_iterator<char>(std::cout), ' ');
+    /** replace */ 
+    replace(sr.begin(), sr.end(), 8, 88); printv(sr);
+    replace_if(sr.begin(), sr.end(),bind(less<int>(), _1, 5), 55); printv(sr);
+    replace_copy_if(sr.begin(), sr.end(),std::ostream_iterator<int>(std::cout, " "), //if / then copy
+        [](int n){return n > 9;}, 99); cout <<endl;
+    /** reverse */ 
+    reverse(begin(sr), end(sr)); printv(sr);
+    reverse_copy(begin(sr), end(sr), to_v.begin()); printv(to_v);
+    /** rotate */ 
+    rotate(sr.begin(), sr.begin() + 1, sr.end());   printv(sr);//rotate left
+    rotate(sr.rbegin(), sr.rbegin() + 1, sr.rend());printv(sr); //rotate right 
+    /** unique */ 
+    auto last = unique(sr.begin(), sr.end()); printv(sr); //delete adjecent element;
+    //sr.erase(last,sr.end());
+    string s1 = "The      string    with many       spaces!";
+    string s2;
+    unique_copy(s1.begin(), s1.end(), back_inserter(s2), //copy if no consecutive duplicates
+            [](char c1, char c2){ return c1 == ' ' && c2 == ' '; });
+    cout << "before: " << s1 << '\n';
+    cout << "after:  " << s2 << '\n';
+    /**  */ 
 
-    int n=2;
-    std::cout << '\n';
-    std::generate(a.begin(), a.end(), [&n]{ return n=n*n; });
-    std::copy(a.cbegin(), a.cend(), std::ostream_iterator<int>(std::cout, " "));
+}
+
+void test_partition(){
+    MARK;
+    /** partion  */ 
+     array<int, 9> v1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    auto is_even = [](int i){ return i % 2 == 0; };
+    cout.setf(ios_base::boolalpha);
+    cout << is_partitioned(v1.begin(), v1.end(), is_even) << endl;
+    partition(v1.begin(), v1.end(), is_even); printv(v1);
+    cout << is_partitioned(v1.begin(), v1.end(), is_even) << endl;
+    int true_arr [5] = {0};    int false_arr [5] = {0};
+    partition_copy(std::begin(v1), std::end(v1), std::begin(true_arr),std::begin(false_arr),
+                        [] (int i) {return i > 5;});
+    printv(true_arr); printv(false_arr);
+
+    std::vector<int> v2{0, 0, 3, 0, 2, 4, 5, 0, 7};
+    stable_partition(v2.begin(), v2.end(), [](int n){return n>3;});  //preserving their relative order
+    printv(v2);
+
+    auto p = std::partition_point(v1.begin(), v1.end(), is_even);  //position to partition
+    std::copy(v1.begin(), p, std::ostream_iterator<int>(std::cout, " "));
+    cout <<endl;
+
+}
+
+void test_sort(){
+    MARK;
+
+
+
+}
+
+
+vector<int> a{0, 1, 2, 3, 4};
+vector<int> b{5, 4, 2, 3, 1};
+
+void test_numeric(){
+    MARK;
+    partial_sum(v.cbegin(), v.cend(), v.begin());
+    cout << "Among the numbers: ";
+        
+    /** product,  */ 
+    int r1 = inner_product(a.begin(), a.end(), b.begin(), 0);
+    cout << "Inner product of a and b: " << r1 << '\n';
+     int r2 = inner_product(a.begin(), a.end(), b.begin(), 0,
+                            plus<int>(), equal_to<int>());
+    cout << "Number of pairwise matches between a and b: " <<  r2 << '\n';
+
+
+}
+void test(){
+    MARK;
+
+}
+
+
+int main(){
+    /**  */ 
+    test_sequantial_access();
+    /**  */ 
+    test_sequantial_modify();
+    /**  */ 
+    test_partition();
+    /**  */ 
+    test_numeric();
+    /**  */ 
+
+
+
+
 }
--- cpp_example/cppProLan/utility.cpp
+++ cpp_example/cppProLan/utility.cpp
@@ -3,127 +3,304 @@
 #include <initializer_list>
 #include <csignal>
 #include <memory>
+#include <utility>
 #include <functional>
 #include <tuple>
 
 using namespace std;
-using namespace std::placeholders;
+using namespace placeholders;
 
-void t_init(initializer_list<int> l) {
+template <typename T> 
+void printv(T &vi){
+    for (auto& x :  vi) // implicit use of v.begin() and v.end()
+        cout << x << "," ; cout << endl;
+}
+
+void t_init(initializer_list<int> l) { 
     cout << "constructed with a " << l.size() << "-element list\n";
-    for (int x : l)cout << x ; cout << '\n';
+    for (int x : l) cout << x << "," ; cout << '\n';
+}
+vector<int> v = {1, 2, 3, 4, 5}; // copy list-initialization
+template <typename T>
+void append(initializer_list<T> l) {
+    v.insert(v.end(), l.begin(), l.end());
 }
 
-void fnoe() noexcept;
-//void baz() noexcept { throw 42; }  
+/**  */ 
+void test_init_list(){
+    printf("-------------init list\n");
+    /** initializer_list */ 
+    for (int x : {-1, -2, -3}) // the rule for auto makes this ranged-for work
+        cout << x << ' '; 
+    cout << '\n';
+    t_init({1,23,3});
+    t_init({1,2,3,4});
+    append({6, 7, 8});      // list-initialization in function call
+    printv(v);
 
-void signal_handler(int signal) 
-{
+}
+
+
+void signal_handler(int signal) {
     if (signal == SIGABRT) {
-        std::cerr << "SIGABRT received\n";
+        cerr << "SIGABRT received\n";
     } 
-    //std::_Exit(EXIT_FAILURE);
-    std::exit(EXIT_FAILURE);
+    if (signal == SIGTERM) {
+        cerr << "SIGTERM received\n";
+    }
+    //_Exit(EXIT_FAILURE);
+    //exit(EXIT_FAILURE);
 }
 
-void f(int n1, int n2, int n3, const int& n4, int n5)
-{
-    std::cout <<"---"<< n1 << ' ' << n2 << ' ' << n3 << ' ' << n4 << ' ' << n5 << '\n';
+/**  */ 
+void test_signal(){
+    printf("-------------signal \n");
+    auto previous_handler = signal(SIGABRT, signal_handler);
+    auto previous_handler1 = signal(SIGTERM, signal_handler);
+    raise(SIGTERM);
+    abort();  // Raise SIGABRT
+
 }
- 
-int g(int n1)
-{
+
+void f(int n1, int n2, int n3, const int& n4, int n5){
+    cout <<"f called: "<< n1 << ' ' << n2 << ' ' << n3 << ' ' << n4 << ' ' << n5 << '\n';
+}
+int g(int n1){
     return n1;
 }
- 
 struct Foo {
-    void print_sum(int n1, int n2)
-    {
-        std::cout << n1+n2 << '\n';
+    int data;
+    Foo(int num = 10) : data(num) {}
+    void print_sum(int n2)  {
+        cout << "sum" << data+n2 << '\n';
     }
-    int data = 10;
 };
+struct func_obj{
+    int operator()(int a, int b){   
+        cout <<"func obj:" << a+b <<endl; 
+        return a + b;   } 
+};
+/**  */ 
 
-std::tuple<double, char, std::string> get_student(int id)
-{
-    if (id == 0) return std::make_tuple(3.8, 'A', "Lisa Simpson");
-    if (id == 1) return std::make_tuple(2.9, 'C', "Milhouse Van Houten");
-    if (id == 2) return std::make_tuple(1.7, 'D', "Ralph Wiggum");
-    throw std::invalid_argument("id");
+void test_bind(){
+    printf("-------------bind\n");
+    int n = 7;
+//1. bind to func
+    auto f1 = bind(f, _2, _1, 42, cref(n), n);
+    f1(1, 2,1001, 1001); //  1001 is unused;  makes a call to f(2, 1, 42, n, 7)
+    n = 10;
+    f1(1, 2);
+    // nested bind subexpressions share the placeholders
+    auto f2 = bind(f, _3, bind(g, _3), _3, 4, 5);
+    f2(10, 11, 12); // makes a call to f(12, g(12), 12, 4, 5)
+    // bind to a pointer to member function
+//2. bind to class member
+    Foo foo;
+    auto f3 = bind(&Foo::print_sum, &foo, _1);
+    f3(5);
+    // bind to a pointer to data member
+    auto f4 = bind(&Foo::data, _1);
+    cout << f4(foo) << '\n';
+
+//3. bind to fun obj
+    cout << "call func obj:" <<bind(func_obj(), _1, _2)(10,20)<<endl; //<int> is return type
+    func_obj af;     
+    cout <<"call ref func obj:"<< bind<int>(ref(af), _1, _2)(11, 22) << endl;     //object ref
+
 }
-struct func{int operator()(int a, int b){   return a + b;   } };
+
+
+/** template< class R, class... Args > * class function<R(Args...)>;
+ * */ 
+void print_num(int i) { cout << i << '\n'; }
+
+void test_function(){
+    printf("-------------bind\n");
+//1. func/ lambda/ bind    
+    //store func
+    function<void(int)> f_display = print_num;
+    f_display(-9);
+    // store a lambda
+    function<void()> f_display_42 = []() { print_num(42); };
+    f_display_42();
+    // store the result of a call to bind
+    function<void()> f_display_31337 = bind(print_num, 31337);
+    f_display_31337();
+//2. class member data/ member func/
+    // store a call to a member function
+    Foo foo(314159);
+    function<void(Foo&, int)> f_add_display = &Foo::print_sum; // 1st;
+    f_add_display(foo, 1);
+    // store a call to a data member accessor 
+    function<int(Foo const&)> f_data = &Foo::data;
+    cout << "data: " << f_data(foo) << '\n';
+    // store a call to a member function and object
+    function<void(int)> f_add_display2 = bind(&Foo::print_sum, foo, _1 ); //2nd
+    f_add_display2(2);
+    // store a call to a member function and object ptr
+    function<void(int)> f_add_display3 = bind(&Foo::print_sum,&foo, _1 ); // 3th
+    f_add_display3(3);
+//3. func obj
+    // store a call to a function object
+    function<void(int,int)> f_display_obj = func_obj();
+    f_display_obj(18,11);
+}
+
+
+
+/**  */ 
+tuple<double, char, string> get_student(int id){
+    if (id == 0) return make_tuple(3.8, 'A', "Lisa Simpson");
+    if (id == 1) return make_tuple(2.9, 'C', "Milhouse Van Houten");
+    if (id == 2) return make_tuple(1.7, 'D', "Ralph Wiggum");
+    throw invalid_argument(" invalid_argument id");
+}
+
+void test_pair_tuple(){
+    printf("-----------pair--tuple \n");
+    /** pair  */ 
+    auto p = make_pair(1, 3.14);
+    cout << '(' << get<0>(p) << ", " << get<1>(p) << ")\n";
+    //cout << '(' << get<int>(p) << ", " << get<double>(p) << ")\n";
+    int n = 11; int a[5] = {1, 2, 3, 4, 5};
+    auto p1 = make_pair(n, a[1]); n = 22; //only value in
+    cout << "p1 is " << "(" << p1.first << ", " << p1.second << ")\n"; //11, 2
+    auto p2 = make_pair(ref(n), a); //
+    n = 33;
+    cout << "p2 is " << "(" << p2.first << ", " << *(p2.second + 2) << ")\n"; //33,3
+    /** tuple */ 
+    auto student0 = get_student(2);
+    cout << "GPA: " << get<0>(student0) << ",   "
+            << "grade: " << get<1>(student0) << ",   "
+            << "name: " << get<2>(student0) << '\n';
+    double gpa1;    char grade1;    string name1;
+    tie(gpa1, grade1, name1) = get_student(1);
+    cout  << "GPA:" << gpa1 << ", g:"  << grade1 << ", n: " << name1 << '\n';
+
+
+}
+
+/**  */ 
+struct A {
+    A(int&& n) { cout << "rvalue overload, n=" << n << "\n"; }
+    A(int& n)  { cout << "lvalue overload, n=" << n << "\n"; }
+};
+class B {
+public:
+    template<class T1, class T2, class T3>
+    B(T1&& t1, T2&& t2, T3&& t3) :
+        a1_{forward<T1>(t1)},
+        a2_{forward<T2>(t2)},
+        a3_{forward<T3>(t3)} {}
+private:
+    A a1_, a2_, a3_;
+};
+ 
+template<class T, class U>
+unique_ptr<T> make_unique1(U&& u){
+    return unique_ptr<T>(new T(forward<U>(u)));
+}
+template<class T, class... U>
+unique_ptr<T> make_unique(U&&... u){
+    return unique_ptr<T>(new T(forward<U>(u)...));
+}
+ 
+
+void test_move_forward(){
+    printf("-------------move forward  \n");
+    string str = "Hello";
+    vector<string> v;
+    v.push_back(move(str)); //After move, str is ""
+    /** forward */ 
+    auto p1 = make_unique1<A>(2); // rvalue
+    int i = 1;
+    auto p2 = make_unique1<A>(i); // lvalue
+    cout << "B\n";
+    auto t = make_unique<B>(2, i, 3); // r, l, r
+}
 
 /**  */ 
 // a non-optimized way of checking for prime numbers:
-#include <future>         // std::async, std::future
-#include <chrono>         // std::chrono::milliseconds
+#include <future>         // async, future
+#include <chrono>         // chrono::milliseconds
 bool is_prime (int x) {
   for (int i=2; i<x; ++i);// if (x%i==0) return false;
   return true;
 }
 
-int test_functional()
+void test_functional()
 {
   // call function asynchronously:
-  try{
-    std::future<bool> fut = std::async (is_prime,444443); 
+    printf("-------------functional \n");
+    try{
+        future<bool> fut = async (is_prime,444443); 
 
-  // do something while waiting for function to set future:
-  std::cout << "checking, please wait";
-  //std::chrono::milliseconds span (100);
-  //while (fut.wait_for(span)==std::future_status::timeout)
-  //  std::cout << '.' << std::flush;
-  bool x = fut.get();     // retrieve return value
-  //std::cout << "\n444444443 " << (x?"is":"is not") << " prime.\n";
-  } catch (std::exception& e) {  
-                  std::cout << "[exceptioncaught: "<< e.what() << "]\n";  
-        } 
-  return 0;
+    // do something while waiting for function to set future:
+    cout << "checking, please wait\n";
+    //chrono::milliseconds span (100);
+    //while (fut.wait_for(span)==future_status::timeout)
+    //  cout << '.' << flush;
+    bool x = fut.get();     // retrieve return value
+    //cout << "\n444444443 " << (x?"is":"is not") << " prime.\n";
+    } catch (exception& e) {  
+        cout << "[exceptioncaught: "<< e.what() << "]\n";  
+    } 
+}
+
+/**  */ 
+void test_time(){
+    printf("-------------time  \n");
+    chrono::system_clock::time_point now = chrono::system_clock::now();
+    time_t now_c = chrono::system_clock::to_time_t(now - chrono::hours(24));
+    printf("24 hours ago, the time was  %s",ctime(&now_c)) ;
+ 
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    cout << "Hello World\n";
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "Printing took "
+              << chrono::duration_cast<chrono::microseconds>(end - start).count() << "us.\n";
+
+    /** chrono::duration */  
+    constexpr auto year = 31556952ll; // seconds in average Gregorian year
+    /** template< Rep, class Period = ratio<1>> class duration
+     * Rep:  type representing the number of ticks ;
+     * Period : a ratio representing the tick period (i.e. the number of seconds per tick) 
+     */
+    using shakes = chrono::duration<int, ratio<1, 100000000>>;
+    using jiffies = chrono::duration<int, centi>; //ratio<1, 100>
+    using microfortnights = chrono::duration<float, ratio<14*24*60*60, 1000000>>;
+    using nanocenturies = chrono::duration<float, ratio<100*year, 1000000000>>;
+ 
+    chrono::seconds sec(1);
+    cout << "1 second is:\n";
+     // integer scale conversion with no precision loss: no cast
+    cout << chrono::microseconds(sec).count() << " microseconds\n"
+              << shakes(sec).count() << " shakes\n"// count	returns the count of ticks
+              << jiffies(sec).count() << " jiffies\n";
+     // integer scale conversion with precision loss: requires a cast
+    cout << chrono::duration_cast<chrono::minutes>(sec).count() << " minutes\n";
+    // floating-point scale conversion: no cast
+    cout << microfortnights(sec).count() << " microfortnights\n"
+              << nanocenturies(sec).count() << " nanocenturies\n";
 }
 
 
 int main()
 {
-    /** initializer_list */ 
-    for (int x : {-1, -2, -3}) // the rule for auto makes this ranged-for work
-        std::cout << x << ' '; 
-    cout << '\n';
-    t_init({1,23,3});
-
     /**  */ 
-    //baz();
-
-    /** abort */ 
-    auto previous_handler = std::signal(SIGABRT, signal_handler);
-    //std::abort();  // Raise SIGABRT
-    
+    test_init_list();
     /** bind */
-    int n = 7;
-    auto f1 = std::bind(f, _2, _1, 42, std::cref(n), n);
-    n = 10;
-    f1(1, 2);
-    auto f2 = std::bind(f, _3, std::bind(g, _3), _3, 4, 5);
-    f2(23,11, 12);
-    // bind to fun obj
-    std::cout << "call func obj:" <<bind(func(), _1, _2)(10,20)<<endl; //<int>表明函数返回类型
-    func af;     
-    std::cout <<"call ref funobj:"<< bind<int>(ref(af), _1, _2)(10, 20) << std::endl;     //对象引用
-
-    /** vector */ 
-    std::string str = "Hello";
-    std::vector<std::string> v;
-    v.push_back(std::move(str));
-    std::cout << "After move, str is \"" << str << "\"\n";
-
+    test_bind();
+    test_function();
     /** tuple */
-    auto student0 = get_student(0);
-    std::cout << "ID: 0, "
-              << "GPA: " << std::get<0>(student0) << ", "
-              << "grade: " << std::get<1>(student0) << ", "
-              << "name: " << std::get<2>(student0) << '\n';
+    test_pair_tuple();
+    test_move_forward();
     /**  */ 
-    printf("-------------\n");
     test_functional();
+    /**  */ 
+    test_time();
+    /**  */ 
+    test_signal();
 
 }
 
